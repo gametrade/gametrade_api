fbc5706019b623da5348a7f83ebb2482b627e8b4
Caio Vitor Santos <caiovitor.santos@gmail.com>
2017-10-28 10:51:52 -0200

    criando rota inicial

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
+  root to: 'games#index'
   resources :users, only: [:show, :update]
   resources :categories, only: [:index, :show]
   resources :game_kinds, only: [:index, :show]
